219bf9429bbe8f2d05edcef5992458d492ff116c
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-12 16:03:46 +0000

    Bishops can move diagonally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -12,9 +12,9 @@ namespace Chessington.GameEngine.Pieces
         {
             var currentSquare = board.FindPiece(this);
             var availableMoves = new List<Square>();
-            for (var i = 0; i < 8; i++)
+            for (var i = 0; i < GameSettings.BoardSize; i++)
                 availableMoves.Add(Square.At(currentSquare.Row, i));
-            for (var i = 0; i < 8; i++)
+            for (var i = 0; i < GameSettings.BoardSize; i++)
                 availableMoves.Add(Square.At(i, currentSquare.Col));
             return availableMoves;
         }
