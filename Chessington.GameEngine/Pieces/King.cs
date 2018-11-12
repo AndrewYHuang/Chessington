b444d451d7223fb80b322a22535ee8aa56f7321e
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-12 18:15:10 +0000

    King can move to all adjacent squares

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var availableMoves = new List<Square>();
+            for (var i = -1; i <= 1; i++)
+            for (var j = -1; j <= 1; j++)
+            {
+                availableMoves.Add(new Square(currentSquare.Row + i, currentSquare.Col + j));
+            }
+
+            availableMoves.RemoveAll(s => !s.IsValid());
+            availableMoves.RemoveAll(s => s == currentSquare);
+            return availableMoves;
         }
     }
 }
